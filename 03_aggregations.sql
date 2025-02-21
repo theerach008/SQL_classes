d23e2b1aa57abab6ec6a924c41b041c8afa872c7
theerach008 <92328023+theerach008@users.noreply.github.com>
2025-02-21 17:18:36 +0530

    Update 03_aggregations.sql

--- 03_aggregations.sql
+++ 03_aggregations.sql
@@ -3,3 +3,19 @@ SELECT COUNT(id) FROM emp;
 SELECT MIN(salary) FROM emp;
 SELECT MAX(salary) FROM emp;
 SELECT AVG(salary) FROM emp;
+
+
+-----------------------------------------------------------------like_clauses.sql-
+SELECT * FROM emp WHERE name LIKE "a%";
+SELECT * FROM emp WHERE name LIKE "%a";
+SELECT * FROM emp WHERE name LIKE "_a%";
+SELECT * FROM emp WHERE name LIKE "%_a";
+SELECT * FROM emp WHERE name LIKE "%a%";
+SELECT * FROM emp WHERE name LIKE "%__a%";
+
+-----------------------------------------------------------------subqueries.sql
+
+SELECT * FROM emp WHERE name = (SELECT name FROM emp WHERE name LIKE 'M%' LIMIT 1);
+SELECT *, CONCAT(name, " is max salary getting person") AS result 
+FROM emp 
+WHERE salary = (SELECT MAX(salary) FROM emp);
