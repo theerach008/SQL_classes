f614fbc6e39496bd5ebc05d1d2e6f17caed85bc0
theerach008 <92328023+theerach008@users.noreply.github.com>
2025-02-21 17:22:57 +0530

    Update and rename 03_aggregations, like, sub_queries.sql to 03_aggregations, like, sub_queries,if .sql

--- 03_aggregations, like, sub_queries,if .sql
+++ 03_aggregations, like, sub_queries,if .sql
@@ -19,3 +19,7 @@ SELECT * FROM emp WHERE name = (SELECT name FROM emp WHERE name LIKE 'M%' LIMIT 
 SELECT *, CONCAT(name, " is max salary getting person") AS result 
 FROM emp 
 WHERE salary = (SELECT MAX(salary) FROM emp);
+
+------------------------------------------------------------------IF
+SELECT experience, IF(experience > 3, "Not less than 3", "Less than 3") FROM emp ORDER BY experience;
+SELECT experience, IF(id = 4, "ID is equal to 4", "ID is not equal 4") FROM emp;
